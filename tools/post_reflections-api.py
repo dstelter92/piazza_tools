5a77d84de64b77d424096196012c69cf451bc05f
dstelter92 <dstelter@bu.edu>
2018-09-20 15:44:24 -0400

    added catch and default values if no groups are present

--- tools/post_reflections-api.py
+++ tools/post_reflections-api.py
@@ -17,11 +17,11 @@ parser.add_argument('-e', '--email', metavar='email', type=str, nargs='?',
 parser.add_argument('-t', '--title', metavar='title', type=str, nargs='*',
         help='Title of post, gets appended with discussion section')
 parser.add_argument('-n', '--ngroups', metavar='ngroups', type=int,
-        help='Number of discussion sections')
+        help='Number of discussion sections', default=1)
 parser.add_argument('-s', '--status', metavar='status', type=str, nargs='?',
-        help='Activity of posts (active, private)')
+        help='Activity of posts (active, private)', default='active')
 parser.add_argument('-g', '--groupname', metavar='groupname', type=str, nargs='?',
-        help='Basename of each group')
+        help='Basename of each group', default='')
 
 
 args = parser.parse_args()
@@ -32,9 +32,9 @@ nsects = args.ngroups
 active = args.status
 groupname = args.groupname
 
-content = "What chemistry topics were new to you this past week? What was"\
+content = "What chemistry topics were new to you this past week? What was "\
         "confusing?\n\n Many students have the same confusions, so take a "\
-        "look at what your peers have answered. See something you understand"\
+        "look at what your peers have answered. See something you understand "\
         "that they don't? Start a discussion! See something you are also "\
         "confused about? Tell them why!\n\n Post your response in this thread"\
         " before Tuesday's lecture.\n\n "
@@ -50,16 +50,25 @@ print '  Logged in as:', me.get('name')
 print ''
 
 for i in range(nsects):
-    disc = groupname + ' ' + str(i+1)
     thistitle = '{} {}{}'.format(title,groupname,i+1)
-    params = {
-        'status': active,
-        'type': 'note',
-        'folders': [disc],
-        'subject': thistitle,
-        'content': content,
-        'config': {'feed_groups': disc.lower() + '_' + classid}
+    if (groupname != ''):
+        disc = groupname + ' ' + str(i+1)
+        params = {
+            'status': active,
+            'type': 'note',
+            'folders': [disc],
+            'subject': thistitle,
+            'content': content,
+            'config': {'feed_groups': disc.lower() + '_' + classid}
         }
+    else:
+        params = {
+            'status': active,
+            'type': 'note',
+            'subject': thistitle,
+            'content': content,
+        }
+
     try:
         pclass._rpc.content_create(params)
     except piazza_api.exceptions.RequestError:
